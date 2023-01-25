acc9ddb69b7881158d0b5f4d3fa15f5c09c1051d
Isaka Developer <isaxstarisaka@gmail.com>
2023-01-25 06:44:28 +0300

    custom response message added

---
+++ customer-service/src/main/java/com/project19/customer/message/ResponseMessage.java
@@ -0,0 +1,10 @@
+package com.project19.customer.message;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ResponseMessage {
+  private String message;
+}
---
+++ payment-service/src/main/java/com/project19/payment/message/ResponseMessage.java
@@ -0,0 +1,10 @@
+package com.project19.payment.message;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ResponseMessage {
+  private String message;
+}
